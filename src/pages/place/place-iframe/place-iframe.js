e3bf618ba77097a4282bf7963105be515ce5de7b
Bogdan Adrian <bogdan4adrian@gmail.com>
2020-03-01 02:03:46 +0100

    Update place-iframe.js

--- src/pages/place/place-iframe/place-iframe.js
+++ src/pages/place/place-iframe/place-iframe.js
@@ -5,7 +5,7 @@ import { useParams } from 'react-router-dom';
 import { createStructuredSelector } from 'reselect';
 import { startFetch } from '../../../redux/fetchPlace/fetchPlace-action';
 import { selectPlaceItem } from '../../../redux/fetchPlace/fetchPlace-selector';
-const api = 'AIzaSyCclgIF4D1AwftMA57ZoPFG0lmSF5XOJW4';
+
 
 const PlaceIframe = ({ startFetch, place, ...props }) => {
   //const env = runtimeEnv();
