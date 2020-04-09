f5df2d41778a36ee50f0eb8daa9d0212d82a2039
Bogdan Adrian <bogdan4adrian@gmail.com>
2020-04-09 15:09:57 +0200

    Updates

---
+++ precache-manifest.2b8dd395b097f54f38129cbab7b984df.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "944c21e2380fcac8225c07ddb59bb35c",
+    "url": "/park-your-tir-frontend/index.html"
+  },
+  {
+    "revision": "59c1bf18e6d6198bbf98",
+    "url": "/park-your-tir-frontend/static/css/10.c8d299c6.chunk.css"
+  },
+  {
+    "revision": "d615724abbea37dabff5",
+    "url": "/park-your-tir-frontend/static/css/11.66ddc67e.chunk.css"
+  },
+  {
+    "revision": "21bbf5289718aa7278bd",
+    "url": "/park-your-tir-frontend/static/css/12.31cd26f0.chunk.css"
+  },
+  {
+    "revision": "8ba2103046113f3be744",
+    "url": "/park-your-tir-frontend/static/css/14.b3ff1c62.chunk.css"
+  },
+  {
+    "revision": "e92c522c47264bd8c3da",
+    "url": "/park-your-tir-frontend/static/css/15.64d2a8bd.chunk.css"
+  },
+  {
+    "revision": "c14c1b5536029f28eedf",
+    "url": "/park-your-tir-frontend/static/css/4.baba500a.chunk.css"
+  },
+  {
+    "revision": "affe49a2fc837b5626dc",
+    "url": "/park-your-tir-frontend/static/css/6.b64cba94.chunk.css"
+  },
+  {
+    "revision": "2011e37ba26d5382d1ea",
+    "url": "/park-your-tir-frontend/static/css/7.28534e71.chunk.css"
+  },
+  {
+    "revision": "25705edc8a96d0c99c85",
+    "url": "/park-your-tir-frontend/static/css/8.2254948d.chunk.css"
+  },
+  {
+    "revision": "8ed4654b20a6a33ed285",
+    "url": "/park-your-tir-frontend/static/css/9.b9a577db.chunk.css"
+  },
+  {
+    "revision": "2f2349c01c6d54586f95",
+    "url": "/park-your-tir-frontend/static/css/main.0e5c9c19.chunk.css"
+  },
+  {
+    "revision": "416343f84627cdbad4d1",
+    "url": "/park-your-tir-frontend/static/js/0.488813a5.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/park-your-tir-frontend/static/js/0.488813a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59c1bf18e6d6198bbf98",
+    "url": "/park-your-tir-frontend/static/js/10.09e44c68.chunk.js"
+  },
+  {
+    "revision": "d615724abbea37dabff5",
+    "url": "/park-your-tir-frontend/static/js/11.418db240.chunk.js"
+  },
+  {
+    "revision": "21bbf5289718aa7278bd",
+    "url": "/park-your-tir-frontend/static/js/12.c2f6ff46.chunk.js"
+  },
+  {
+    "revision": "f1a07198dc1e6af36ec4",
+    "url": "/park-your-tir-frontend/static/js/13.69d9b125.chunk.js"
+  },
+  {
+    "revision": "c46009a87f85d42f9c995bc337df230c",
+    "url": "/park-your-tir-frontend/static/js/13.69d9b125.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ba2103046113f3be744",
+    "url": "/park-your-tir-frontend/static/js/14.e23c5851.chunk.js"
+  },
+  {
+    "revision": "e92c522c47264bd8c3da",
+    "url": "/park-your-tir-frontend/static/js/15.77a248cf.chunk.js"
+  },
+  {
+    "revision": "973c555feb62ecf29bcd",
+    "url": "/park-your-tir-frontend/static/js/3.d8213d10.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/park-your-tir-frontend/static/js/3.d8213d10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c14c1b5536029f28eedf",
+    "url": "/park-your-tir-frontend/static/js/4.078ea98c.chunk.js"
+  },
+  {
+    "revision": "c8f7ac8ce49825080810",
+    "url": "/park-your-tir-frontend/static/js/5.110b7002.chunk.js"
+  },
+  {
+    "revision": "9acdaa7cdbf2556c351b3d3b5065380f",
+    "url": "/park-your-tir-frontend/static/js/5.110b7002.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "affe49a2fc837b5626dc",
+    "url": "/park-your-tir-frontend/static/js/6.a2efe7dc.chunk.js"
+  },
+  {
+    "revision": "2011e37ba26d5382d1ea",
+    "url": "/park-your-tir-frontend/static/js/7.59f147c6.chunk.js"
+  },
+  {
+    "revision": "25705edc8a96d0c99c85",
+    "url": "/park-your-tir-frontend/static/js/8.e722e857.chunk.js"
+  },
+  {
+    "revision": "8ed4654b20a6a33ed285",
+    "url": "/park-your-tir-frontend/static/js/9.645ddf71.chunk.js"
+  },
+  {
+    "revision": "2f2349c01c6d54586f95",
+    "url": "/park-your-tir-frontend/static/js/main.24f0c8c8.chunk.js"
+  },
+  {
+    "revision": "72f5c9c18fa0b45addbf",
+    "url": "/park-your-tir-frontend/static/js/runtime-main.b2bbb488.js"
+  },
+  {
+    "revision": "986a150ca8ee44185d824fe342ad3a52",
+    "url": "/park-your-tir-frontend/static/media/truck.986a150c.svg"
+  },
+  {
+    "revision": "cd9fe60722eeb54a8f389a4d68ba1286",
+    "url": "/park-your-tir-frontend/static/media/truck.cd9fe607.png"
+  }
+]);
